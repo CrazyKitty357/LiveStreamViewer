9400407e288c3d49cd4779119513e6f37baca761
CrazyKitty357 <73915693+CrazyKitty357@users.noreply.github.com>
2023-05-14 21:58:37 -0400

    Add files via upload

---
+++ update.ps1
@@ -0,0 +1,24 @@
+param (
+    [Parameter(Mandatory=$false)]
+    [string]$bat
+)
+$host.ui.RawUI.WindowTitle = "LiveStreamViewer Updater"
+Write-Host "Do you wish to update LiveStreamViewer? [Y/N]"
+$updateCONF = Read-Host
+switch -Regex ($updateCONF) {
+'Y|yes' {Write-Host "updating..."
+    Remove-Item -Path "LiveStreamViewer.ps1"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/CrazyKitty357/LiveStreamViewer/main/LiveStreamViewer.ps1" -OutFile "LiveStreamViewer.ps1" 
+    Write-Host "The update was successful."
+    Write-Host "You can view the changelog by typing 'changelog' in LiveStreamViewer."
+    Pause
+    Write-Host "Restarting..."
+    Start-Sleep -Milliseconds 3500
+    Clear-Host
+    Powershell -File "LiveStreamViewer.ps1" $bat
+}
+'N|no' {Write-Host "Returning to main program..."
+    Start-Sleep -Milliseconds 2000
+    Clear-Host
+    powershell -File "LiveStreamViewer.ps1" $bat}
+}
